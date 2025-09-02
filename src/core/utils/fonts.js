82c1176d7398227d747c6aea0fc18f109c255a5d
mohmdrezaei <mhwmdrz9@gmail.com>
2025-09-03 01:26:19 +0330

    Add persian font

---
+++ src/core/utils/fonts.js
@@ -0,0 +1,52 @@
+import localFont from "next/font/local";
+
+export const peyda = localFont({
+    src: [
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-Thin.woff2",
+        weight: "100",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-ExtraLight.woff2",
+        weight: "200",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-Light.woff2",
+        weight: "300",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-Regular.woff2",
+        weight: "400",
+        style: "normal",
+      },
+
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-Medium.woff2",
+        weight: "500",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-SemiBold.woff2",
+        weight: "600",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-Bold.woff2",
+        weight: "700",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-ExtraBold.woff2",
+        weight: "800",
+        style: "normal",
+      },
+      {
+        path: "../../../public/fonts/woff2/PeydaWebFaNum-Black.woff2",
+        weight: "900",
+        style: "normal",
+      },
+    ],
+  });
