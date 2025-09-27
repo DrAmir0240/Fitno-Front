f72579f5f3896070803cdda5a98ee77e14957842
mohmdrezaei <mhwmdrz9@gmail.com>
2025-09-27 13:10:10 +0330

    Invalidate user data after login

--- src/services/mutation/auth.js
+++ src/services/mutation/auth.js
@@ -1,13 +1,23 @@
 import api from "@/config/api";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const useRegister = () => {
+  const queryClient = useQueryClient()
   const mutationFn = (data) => api.post("accounts/customer/register/", data);
-  return useMutation({ mutationFn });
+  return useMutation({ mutationFn , 
+    onSuccess: (response) => {
+     queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+   });
 };
 const useLogin = () => {
+  const queryClient = useQueryClient()
   const mutationFn = (data) => api.post("accounts/login/", data);
-  return useMutation({ mutationFn });
+  return useMutation({ mutationFn , 
+    onSuccess: (response) => {
+     queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+   });
 };
 
 export { useRegister, useLogin };
